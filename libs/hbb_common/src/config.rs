39041951d2ab2c1348678b6c94536f4de6575d6e
cnsdjnwq <128951176+cnsdjnwq@users.noreply.github.com>
2024-11-03 22:17:15 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["remotecn.tomwu.asia"];
-pub const PUBLIC_RS_PUB_KEY: &str = "X9YQPOYWNwvYYD8uK0aGv7GkKGQzEa8uPE1bCJUlN8s=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["remoteus.tomwu.asia"];
+pub const PUBLIC_RS_PUB_KEY: &str = "gMmXrzdwT1f5LLBmtUmIR0OfDsrakEfSoRhkJ842J+E=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
